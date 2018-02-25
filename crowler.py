94de31a3b7da271cef473644d5b1e3511b8d11d2
Satyendra Pandey <satendrapandeymp@gmail.com>
2018-02-25 18:59:26 +0530

    Added support for url.txt

--- crowler.py
+++ crowler.py
@@ -31,10 +31,10 @@ browser = webdriver.PhantomJS()
 def google_links(page):
 	res = []
 	browser.get(page)
-	time.sleep(1)
+	time.sleep(2)
 	for _ in range(5):
 	    browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-	    time.sleep(1)
+	    time.sleep(2)
 	Page_Html = browser.page_source
 
 	Parsed_html = Soup(Page_Html, "html.parser")
@@ -48,11 +48,7 @@ def google_links(page):
 			link = link.replace("%3A", ":")
 			link = link.replace("%2F", "/")
 			link = link.replace("%2520", "%20")
-
-			name = link.split("://")[1].split("/")[0]
-			name = name + "_" + str(random.randint(1,999)) + "." + link.split(".")[-1]
-			temp = {"link":link, "name":name, "original":original}
-			res.append(temp)
+			res.append(link)
 		except:
 			print "Found something not captured"
 
@@ -66,22 +62,38 @@ def download(name, arr, num):
 
 	for doc in arr:
 		count += 1
-		link = doc["link"]
+		link = doc
 		link = cleanupString(link)
-		file_name = name + "/" + doc["name"]
+		link = link.replace(" ", "%20")
+		filename = link.split("://")[1].split("/")[0]
+		filename = filename + "_" + str(random.randint(1,999)) + "." + link.split(".")[-1].split("%")[0]
+		file_name = name + "/" + filename
 		try:
-			test = opener.open(link)
+			test = opener.open(link, timeout=5)
 			with open(file_name, 'wb') as file:
 				file.write(test.read())
 		except:
-			print link, doc["original"]
+			print link
 		if count > num:
 			break
 
-seed=raw_input('Enter the keywords : ')
-num=int(raw_input('No of Images you want -- : '))
+check =raw_input('Do you have urls.txt ? y/n : ')
 
-query = "https://www.google.co.in/search?client=ubuntu&hs=JQU&channel=fs&dcr=0&source=lnms&tbm=isch&sa=X&ved=0ahUKEwi4hammhsDZAhXBL48KHVVRDtsQ_AUICygC&biw=1366&bih=662" + "+".join(seed.split(' '))
-result = google_links(query)
-print len(result)
+if check.lower() == 'y':
+	seed=raw_input('Meks sure your filename is in this format -: \nurl1\nurl2\n...\nEnter PATH to Filename : ')
+	file = open(seed, 'r')
+	result = file.read().split('\n')
+	num = len(result)
+	seed = seed.split(".")[0]
+
+else:
+	seed=raw_input('Enter the keywords : ')
+	num=int(raw_input('No of Images you want -- : '))
+	query = "https://www.google.com/search?tbm=isch&source=hp&biw=1366&bih=662&ei=qIOSWuyqNMH-vASAs43YDg&gs_l=img.3.0.35i39k1l2j0l8.12015.13207.0.14351.9.9.0.0.0.0.152.847.0j6.6.0....0...1ac.1.64.img..3.6.843.0...0._9c6L5S5nY0&q=" + "+".join(seed.split(' '))
+	result = google_links(query)
+
+	with open(seed+".txt", 'wb') as writer:
+		for url in result:
+			writer.write(url + "\n")
+
 download(seed, result,num)
